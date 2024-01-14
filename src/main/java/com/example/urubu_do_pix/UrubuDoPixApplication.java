3e2f461c9cd434245a52761101cf79169ff56047
Lucas Voltera <lucasvoltera@gmail.com>
2024-01-14 20:51:21 -0300

    feat: first commit

---
+++ src/main/java/com/example/urubu_do_pix/UrubuDoPixApplication.java
@@ -0,0 +1,13 @@
+package com.example.urubu_do_pix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrubuDoPixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrubuDoPixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/urubu_do_pix/UrubuDoPixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urubu_do_pix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrubuDoPixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
